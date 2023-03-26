52e7fbeb9dcde8b2bceedb270c093c74c0ec754c
sr-josh <dev.sr.jo2@gmail.com>
2023-03-26 20:24:17 +0900

    more type added

--- script/script.js
+++ script/script.js
@@ -1,32 +1,44 @@
 $(function() {
   //popup
   $('.notice > li:nth-child(1)').click(function() {
-    $('.popup-box').show()
-    $('.popup-bg').show()
+    $('.popup-box', '.popup-bg').show()
   })
 
   $('.popup-box button').click(function() {
-    $('.popup-box').hide()
-    $('.popup-bg').hide()
+    $('.popup-box', '.popup-bg').hide()
   })
 
-  //menu A
-  $('header nav > ul').mouseenter(function(){
-    $('ul.sub-menu').stop().slideDown(800)
-  });  
+  //menu 가로1 2
+  // $('header nav > ul').mouseenter(function() {
+  //   $('ul.sub-menu, .header-bg').stop().slideDown()
+    
+  // });  
 
-  $('header nav > ul').mouseleave(function(){
-    $('ul.sub-menu').stop().slideUp(500)
+  // $('header nav > ul').mouseleave(function() {
+  //   $('ul.sub-menu, .header-bg').stop().slideUp()
+  // })
+
+  //menu 가로3 세로1
+  $('li.menu').mouseenter(function() {
+    // $(this).find('ul.sub-menu').stop().slideDown()
+    $(this).find('ul.sub-menu').stop().fadeIn() //세로2
+  })
+  $('li.menu').mouseleave(function() {
+    // $(this).find('ul.sub-menu').stop().slideUp()
+    $(this).find('ul.sub-menu').stop().fadeOut()//세로2 
   })
 
-  //slide
-  setInterval(function() { // 가로는 margin-left
+  
+  
+
+  //slide-세로 / 가로는 margin-left
+  setInterval(function() { 
     $('.slide-wrap').animate({'margin-top':'-300px'}, function() { 
       $('.slide:first').appendTo('.slide-wrap')
       $('.slide-wrap').css({'margin-top':'0px'})
     })
 
-  // 페이드
+  // fade
   // $('.slide:gt(0)').hide()
   // setInterval(function() {
   //   $('.slide:first').fadeOut(1500).next().fadeIn(1500)
