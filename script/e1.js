f72bc943c127daa78e21b528c3ca01fb339efb1f
josh <josh@malgnsoft.com>
2023-08-04 14:18:13 +0900

    siro

--- script/e1.js
+++ script/e1.js
@@ -11,4 +11,14 @@ $(function() {
         $('.popup-box').hide();
     });
 
+  setInterval(function() { 
+    // fade $('.slide:gt(0)').hide()
+    $('.slide-wrap').animate({'margin-left':'-300px'}, function() { 
+      // fade $('.slide:first').fadeOut(1500).next().fadeIn(1500)
+      $('.slide:first').appendTo('.slide-wrap')
+      $('.slide-wrap').css({'margin-left':'0px'})
+    })
+  }, 3000)
+
+
 });
